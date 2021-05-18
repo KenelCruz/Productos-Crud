e2962cc4d30e0def9ba99af4874eab9074381c83
Kenel Cruz <kenelcruz@gmail.com>
2021-05-18 11:01:36 -0400

    Impotacion de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
+
+
+import { AppRoutingModule, routinComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavarComponent } from './Nav/navar/navar.component';
+import { ViewproductoComponent } from './Vistas/ViewPrducto/viewproducto/viewproducto.component';
+import { PerfilComponent } from './Vistas/Perfil/perfil/perfil.component';
+//import { ProductosComponent } from './Vistas/Productos/productos/productos.component';
+//import { AddproductoComponent } from './Vistas/AddProducto/addproducto/addproducto.component';
+//import { EditproductoComponent } from './Vistas/EditProducto/editproducto/editproducto.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavarComponent,
+    routinComponents,
+    ViewproductoComponent,
+    PerfilComponent,
+    //ProductosComponent,
+    //AddproductoComponent,
+    //EditproductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
