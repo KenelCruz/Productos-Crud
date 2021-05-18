045815d773e746b9f1dc0503e3539b82378728be
Kenel Cruz <kenelcruz@gmail.com>
2021-05-18 11:02:44 -0400

    Diseño y rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiproductoService } from 'src/app/Servicios/apiproducto.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Productos-Crud';
+
+  constructor(private ApiproductoService: ApiproductoService){
+
+  }
 }
